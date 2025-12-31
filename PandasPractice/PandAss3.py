8d4d27e7fc8f3d9be94369f0795884e24beb10c9
AmnaAkhtar44 <akhtaramna752@gmail.com>
2025-12-31 18:57:26 +0500

    Pandas practices done

--- PandasPractice/PandAss3.py
+++ PandasPractice/PandAss3.py
@@ -112,8 +112,9 @@ print("# Case 2 : using .loc with index_col - starts here")
 # Case 2 : using .loc with index_col - starts here
 # Second cycle - with index_col as property_id
 # Why Second cycle - Note Index - , index_col='property_id
-df_index_col = pd.read_csv('Amna-AI-Course-Bin/PandasPractice/Startups in 2021 end.csv',delimiter=";",parse_dates=[14], date_format={'date_added': '%d-%m-%Y'} , index_col='SpaceX')
+df_index_col = pd.read_csv('Amna-AI-Course-Bin/PandasPractice/Startups in 2021 end.csv',delimiter=",",parse_dates=['Date Joined'] ,index_col=0)#parse_dates=[14], date_format={'date_added': '%d-%m-%Y'} , index_col=0)
 print(df_index_col)
+print(df_index_col.head())
 print(df_index_col.dtypes)
 print(df_index_col.info())
 
@@ -123,23 +124,49 @@ second_row = df_index_col.loc[200]
 print("Access single row")
 print(second_row)
 print(df)
-#Selecting multiple rows using .loc
 
+#Selecting multiple rows using .loc
+second_row1 = df_index_col.loc[[451 , 551]]
+print("Access multiple Rows")
+print(second_row1)
+print(df)
 
 #Selecting a slice of rows using .loc
+second_row2 = df_index_col.loc[250:700]
+print("Access slice of columns")
+print(second_row2)
+print(df)
 
 #Conditional selection of rows using .loc
+second_row4 = df_index_col.loc[df_index_col['Company'] == 'Gateway Properties']
+print("#Conditional selection of rows using .loc")
+print(second_row4)
+print()
 
 #Selecting a single column using .loc
-
+second_row5 = df_index_col.loc[:550,'City']
+print("#Selecting a single column using .loc")
+print(second_row5)
+print()
 
 
 #Selecting multiple columns using .loc
-
+second_row6 = df_index_col.loc[:550,['Country','City']]
+print("#Selecting multiple columns using .loc")
+print(second_row6)
+print()
 
 #Selecting a slice of columns using .loc
+second_row7 = df_index_col.loc[:550,'Country' : 'Industry']
+print("#Selecting multiple columns using .loc")
+print(second_row7)
+print()
 
 #Combined row and column selection using .loc   # columns whose names are gateway prop.
+second_row8 = df_index_col.loc[df_index_col['Company'] == 'Gateway Properties','Country':'Industry']
+print("#Combined row and column selection using .loc")
+print(second_row8)
+print()
 
 # Case 2 : using .loc with index_col  -  ends here
 
@@ -153,23 +180,47 @@ Counting starting at 0: The first row and column have the index 0, the second on
 Exclusivity of range end value: When using a slice, the row or column specified behind the colon is not included in the selection."""
 
 #Selecting a single row using .iloc
-
+second_row = df_index_col.iloc[0]
+print("#Selecting a single row using .iloc")
+print(second_row)
+print()
 
 #Selecting multiple rows using .iloc
-
+second_row2 = df_index_col.iloc[[1, 3,5]]
+print("#Selecting multiple rows using .iloc")
+print(second_row2)
+print()
 
 #Selecting a slice of rows using .iloc
-
+second_row3 = df_index_col.iloc[2:5]
+print("#Selecting a slice of rows using .iloc")
+print(second_row3)
+print()
 
 #Selecting a single column using .iloc
-
+second_row5 = df_index_col.iloc[:,2]
+print("#Selecting a single column using .iloc")
+print(second_row5)
+print()
 
 #Selecting multiple columns using .iloc
+second_row6 = df_index_col.iloc[:,[2,4]]
+print("#Selecting multiple columns using .iloc")
+print(second_row6)
+print()
+
+#Selecting a slice of columns using .iloc
+second_row7 = df_index_col.iloc[:,2:4]
+print("#Selecting a slice of columns using .iloc")
+print(second_row7)
+print()
 
 #Combined row and column selection using .iloc
+second_row8 = df_index_col.iloc[[1, 3,5],2:4]
+print("#Combined row and column selection using .iloc")
+print(second_row8)
+print()
 
-
-# Case 3 : Using .iloc - ends here
 
 # Next Run 
 print("Next Run")
@@ -186,45 +237,59 @@ Renaming rows/columns"""
 # Copy array from list and add to DataFrame
 # 3477952;82;"https://www.zameen.com/Property/lahore_model_town_6_kanal_excellent_house_for_sale_in_model_town-347795-8-12.html";"House2";2200000002;"Model Town2";"Lahore2";"Punjab2";312.483868658082;742.325685501099;02;"6 Kanal2";"For Sale2";02;"07-17-2019";"Real Biz International2";"Usama Khan2"
 
-#df.loc[len(df.index)] = 
+#df.loc[len(df.index)] = [157,Impossible Foods,$4,5/13/2019,United States,Redwood City,Consumer & retail,"Khosla Ventures, Horizons Ventures,Temasek Holdings"]
+print("Modified DataFrame - add a new row:")
+print(df)
+print()
+
 
 
 #Remove Rows/Columns from a Pandas DataFrame
 
 
-# delete row with index 1  # Axis 0 determine row and drop row 1
+df.drop(1, axis=0, inplace=True)
 # delete row with index 1
+df.drop(index=2, inplace=True)
 # delete rows with index 3 and 5
+df.drop([3, 5], axis=0, inplace=True)
 # display the modified DataFrame after deleting rows
+print("Modified DataFrame - Remove Rows:")
+print(df)
 
 
 
 # delete age column # Axis 1 means column
- # inplace means to drop from given df otherwise we should store modified df after inplaced df in other variable
+df.drop('Company', axis=1, inplace=True) # inplace means to drop from given df otherwise we should store modified df after inplaced df in other variable
 # delete marital status column
+df.drop(columns='City', inplace=True)
 # delete height and profession columns
+df.drop(['Country', 'Industry'], axis=1, inplace=True)
 # display the modified DataFrame after deleting rows
-
-
+print("Modified DataFrame -  delete page_url ,property_type , location , city , column :")
+print(df)
 
 #Rename Labels in a DataFrame
 # rename column 'Name' to 'First_Name'
+df.rename(columns= {'Company': 'Company_nameChanged'}, inplace=True)
 # rename columns 'Age' and 'City'
+df.rename(mapper= {'City': 'City_Changed', 'date_added':'date_added_Changed'}, axis=1, inplace=True)
 # display the DataFrame after renaming column
-
+print("Modified DataFrame  - Rename Labels :")
+print(df)
 
 #Example: Rename Row Labels
 # rename column one index label
+df.rename(index={0: 7}, inplace=True)
 # rename columns multiple index labels
+df.rename(mapper={1: 10, 2: 100}, axis=0, inplace=True)
 # display the DataFrame after renaming column
+print("Modified DataFrame - Rename Row - 0  >>> 7 , 1 >>> 10 , 2 >>> 100  Labels:")
+print(df)
 
+#selected_rows = df.query('Valuation ($B) == \'Gateway Properties\' or Valuation ($B) > $25')
 
-
-#query() to Select Data
-#The query() method in Pandas allows you to select data using a more SQL-like syntax.
-
-# select the rows where the age is greater than 25
-
+#print(selected_rows.to_string())
+#print(len(selected_rows))
 
 
 
@@ -233,65 +298,46 @@ Renaming rows/columns"""
 #Sort Pandas DataFrame by Multiple Columns
 
 # 1. Sort DataFrame by 'Age' and then by 'Score' (Both in ascending order)
+#df1 = df.sort_values(by=['City', 'Industry'])
 
+print("Sorting by 'price' (ascending) and then by 'location_id' (ascending):\n")
+#print(df1.to_string(index=False))
 
+#grouped = df.groupby('City')['Industry'].sum()
 
-"""
-Reference: https://www.geeksforgeeks.org/python-pandas-dataframe-to_string/
-Explanation: This code creates a DataFrame from a dictionary with three columns (Weight, Name, Age), structures it into a tabular format using pd.DataFrame() and converts it into a fully visible string representation with df.to_string().
-
-Syntax
-DataFrame.to_string(buf=None, columns=None, col_space=None, header=True, index=True, na_rep=’NaN’, formatters=None, float_format=None, sparsify=None, index_names=True, justify=None, max_rows=None, max_cols=None, show_dimensions=False, decimal=’.’, line_width=None)
-
-
-Parameters:
-
-buf: Buffer to write the output string to (e.g., a file). Defaults to None, which means the output is returned as a string.
-columns: Specifies a subset of columns to include in the output. If None, all columns are printed.
-col_space: Defines the minimum width of each column.
-header: Whether to print column names. Can also accept a list of column name aliases.
-index: Whether to include index labels. Default is True.
-na_rep: String representation for missing values (NaN). Default is ‘NaN’.
-formatters: Dictionary or list of functions to apply to columns for formatting their output.
-float_format: Formatter function to apply specifically to floating-point numbers.
-sparsify: Controls hierarchical index formatting. If False, prints every multi-index key at each row.
-index_names: Whether to print index names. Default is True.
-justify: Alignment of column headers (‘left’, ‘right’, ‘center’, ‘justify’ or ‘justify-all’).
-max_rows: Maximum number of rows to display. If exceeded, truncates output.
-max_cols: Maximum number of columns to display. If exceeded, truncates output.
-show_dimensions: If True, displays the shape (rows x columns) of the DataFrame.
-decimal: Specifies the character for decimal separation (e.g., ‘,’ for European formatting).
-line_width: Defines the maximum character width of a row before wrapping text."""
-
-
-
-#Pandas groupby
-#In Pandas, the groupby operation lets us group data based on specific columns. This means we can divide a DataFrame into smaller groups based on the values in these columns.
-
-# group the DataFrame by the location_id column and
-# calculate the sum of price for each category
-
+#print(grouped.to_string())
+#print("grouped :" , len(grouped))
 
 
 """"Pandas Data Cleaning
 Data cleaning means fixing and organizing messy data. Pandas offers a wide range of tools and functions to help us clean and preprocess our data effectively.
 """
 # use dropna() to remove rows with any missing values
+df_cleaned = df.dropna()
+print("Cleaned Data:\n",df_cleaned)
 
 
 # filling NaN values with 0
+df.fillna(0, inplace=True)
 
+print("\nData after filling NaN with 0:\n", df)
 
 
 
 # create a list named data
+data = [2, 4, 6, 8]
 # create Pandas array using data
+array1 = pd.array(data)
+print(array1)
 """<IntegerArray>
 [2, 4, 6, 8]
 Length: 4, dtype: Int64"""
 
 
 # creating a pandas.array of integers
+int_array = pd.array([1, 2, 3, 4, 5], dtype='int')
+print(int_array)
+print()
 
 #Pandas Reshape
 #In Pandas, reshaping data refers to the process of converting a DataFrame from one format to another for better data visualization and analysis.
@@ -299,3 +345,6 @@ Length: 4, dtype: Int64"""
 
 
 # to be continued....
+
+
+
